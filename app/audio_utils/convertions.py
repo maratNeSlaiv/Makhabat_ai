e2f6a89d8d6bed17d449fad65590454dee5a5232
maratNeSlaiv <maratorozaliev2004@gmail.com>
2024-11-14 20:42:29 +1100

    nothing

--- app/audio_utils/convertions.py
+++ app/audio_utils/convertions.py
@@ -1,7 +1,8 @@
+import time
 import io
 import wave
 import numpy as np
-from ..gen_ai_logic.answer_back import convert_audio_to_text
+from app.gen_ai_logic.answer_back import convert_audio_to_text
 
 sample_rate = 44100
 
@@ -22,3 +23,19 @@ def get_text_from_audio(recording):
     
     text_message = convert_audio_to_text(audio_file)
     return text_message
+
+if __name__ == '__main__':
+    from openai import OpenAI
+    client = OpenAI()
+
+    filename = '/Users/maratorozaliev/Desktop/goodto.m4a'
+    audio_file= open(filename, "rb")
+    start_time = time.time()
+    transcription = client.audio.transcriptions.create(
+    model="whisper-1", 
+    file=audio_file,
+    response_format='text'
+    )
+    end_time = time.time()
+    print(transcription)
+    print('Difference:', end_time - start_time)
